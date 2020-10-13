4dd470acb5c811658e01c908f490bc3a44e43c7e
Matejce23 <matej-gjosev@outlook.com>
2020-10-13 11:42:20 +0200

    final task for client

--- upwork/script.js
+++ upwork/script.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+    let productImg = document.getElementsByClassName("product-image");
+    for(item of productImg) {
+        if(item.getAttribute("category") == "blue")
+        {
+            item.classList.remove("show");
+            item.classList.add("hide");
+        }
+        else if(item.getAttribute("category") == "green")
+        {
+            item.classList.remove("show");
+            item.classList.add("hide");
+        }
+        else {
+            item.classList.add("show");
+            item.classList.remove("hide");
+        }
+    }
+  };
 let arr3 = document.querySelectorAll('#cate-ul>ul>li>a');
 for(let i = 0; i < arr3.length;i++) {
     arr3[i].addEventListener("click", setActiveCategoryLi);
